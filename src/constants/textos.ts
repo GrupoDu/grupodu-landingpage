e1fb5ed31cae7cc0c5b5dd9c7b74f80b0b42b7ef
jukalbf <lgbr212@gmail.com>
2025-08-12 16:54:30 -0300

    secao dos cards pronta

--- src/constants/textos.ts
+++ src/constants/textos.ts
@@ -1,3 +1,12 @@
+import {
+  ChevronUp,
+  Headset,
+  Shield,
+  Smile,
+  TrendingUp,
+  UsersRound,
+} from "lucide-react";
+
 export const domMetal = {
   nome: "Dom Metal",
   subtitulo: "Soluções e Carrinhos",
@@ -36,3 +45,42 @@ export const carbuilt = {
     "Projetos Sob Medida",
   ],
 };
+
+export const cards = [
+  {
+    title: "Qualidade Garantida",
+    description:
+      "Todos os nossos serviços seguem rigorosos padrões de qualidade e contam com garantia.",
+    icon: Shield,
+  },
+  {
+    title: "Agilidade",
+    description:
+      "Processos otimizados para entregar resultados no menor tempo possível sem comprometer a qualidade.",
+    icon: ChevronUp,
+  },
+  {
+    title: "Equipe Especializada",
+    description:
+      "Profissionais altamente qualificados e em constante atualização técnica.",
+    icon: UsersRound,
+  },
+  {
+    title: "Suport Completo",
+    description:
+      "Atendimento personalizado e suporte contínuo para todas as necessidades dos clientes.",
+    icon: Headset,
+  },
+  {
+    title: "Experiência Comprovada",
+    description:
+      "Mais de uma década de experiência atendendo clientes com excelência.",
+    icon: Smile,
+  },
+  {
+    title: "Soluções Inovadoras",
+    description:
+      "Sempre em busca de novas tecnologias e metodologias para oferecer o melhor.",
+    icon: TrendingUp,
+  },
+];
