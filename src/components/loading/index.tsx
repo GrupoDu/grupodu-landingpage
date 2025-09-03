86e21851a2753c284b81fe9db488fcbd1bc8258d
jukalbf <lgbr212@gmail.com>
2025-09-03 16:04:21 -0300

    style(loading): mudando tipo de loading

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-import { PulseLoader } from "react-spinners";
+import { BarLoader } from "react-spinners";
 import styles from "./styles.module.scss";
 
-type Props = {
-  size: number;
-};
-
-const Loading = ({ size }: Props) => {
+const Loading = () => {
   return (
     <div className={styles.loading}>
-      <PulseLoader color="white" size={size} />
+      <BarLoader color="white" />
     </div>
   );
 };
