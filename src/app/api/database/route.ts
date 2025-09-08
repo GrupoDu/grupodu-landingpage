99f5d1e7bb155250279850d024a855210fe30951
jukalbf <lgbr212@gmail.com>
2025-09-08 14:28:17 -0300

    feat(database): extraindo dados dos produtos

--- src/app/api/database/route.ts
+++ src/app/api/database/route.ts
@@ -1,10 +1,28 @@
 import { createClient } from "@supabase/supabase-js";
+import { NextRequest, NextResponse } from "next/server";
 
 const supabaseUrl = process.env.SUPABASE_URL || "SUPABASE_URL";
 const supabaseKey = process.env.SUPABASE_API_KEY || "SUPABASE_API_KEY";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export const GET = async (produto: string) =>
-  supabase.from("produtos").select("*").eq("tipo", produto);
+export const GET = async (request: NextRequest) => {
+  const searchParams = request.nextUrl.searchParams;
+  const produto = searchParams.get("produto");
 
+  if (!produto) {
+    return NextResponse.json(
+      { error: 'Parâmetro "produto" é obrigatório' },
+      { status: 400 }
+    );
+  }
+
+  const { data, error } = await supabase
+    .from("produtos")
+    .select("*")
+    .eq("tipo_produto", produto);
+
+  if (error) return NextResponse.error();
+
+  return NextResponse.json(data);
+};
