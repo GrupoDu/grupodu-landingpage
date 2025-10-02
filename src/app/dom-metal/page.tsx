1f8ce8e53bb2bcfa0f70a677170a3b213185084c
João Lucas <lgbr212@gmail.com>
2025-10-02 15:51:19 -0300

    style(dom-metal): topContent finalizado

--- src/app/dom-metal/page.tsx
+++ src/app/dom-metal/page.tsx
@@ -1,11 +1,30 @@
+// libs
 import React from "react";
 import styles from "./page.module.scss";
+import Image from "next/image";
+
+// Import de imagens
+import Logo from "../../../public/Logo dom metal.png";
+
+const ProofPoints = ({ proof }: { proof: string }) => {
+  return (
+    <div className={styles.proofPoints}>
+      <p>{proof}</p>
+    </div>
+  );
+};
 
 const DomMetalPage = () => {
   return (
     <div className={styles.pageContainer}>
       <div className={styles.heroSection}>
-        
+        <div className={styles.topContent}>
+          <Image src={Logo} alt={"logo dom metal"} className={styles.logo} />
+          <h1>
+            Fabricante de Carros de mão, Plataformas de Trabalho e Estruturas
+            Metálicas para Construção Civil
+          </h1>
+        </div>
       </div>
     </div>
   );
