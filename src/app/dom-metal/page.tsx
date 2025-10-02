06b3d8e1adb6fe094f5fb76b582aae8a7d28bd2e
João Lucas <lgbr212@gmail.com>
2025-10-02 15:14:51 -0300

    chore(dom-metal): criando pagina para dom metal

---
+++ src/app/dom-metal/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./page.module.scss";
+
+const DomMetalPage = () => {
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.heroSection}>
+        
+      </div>
+    </div>
+  );
+};
+
+export default DomMetalPage;
