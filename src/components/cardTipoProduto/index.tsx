be67fe88513b62d631eebe9be917bf2ff574361a
João Lucas <lgbr212@gmail.com>
2025-10-02 16:53:41 -0300

    chore(cardTipoProduto): importando template de imagem

--- src/components/cardTipoProduto/index.tsx
+++ src/components/cardTipoProduto/index.tsx
@@ -1,6 +1,7 @@
 import { StaticImageData } from "next/image";
 import styles from "./styles.module.scss";
 import React from "react";
+import { ImageIcon } from "lucide-react";
 
 type Props = {
   titulo: string;
@@ -9,8 +10,22 @@ type Props = {
   buttonScheme: string;
 };
 
+const ImageTemplate = () => {
+  return (
+    <div className={styles.imageTemplate}>
+      <ImageIcon opacity={0.4} />
+    </div>
+  );
+};
+
 const CardTipoProduto = ({ titulo, bullets, imagem, buttonScheme }: Props) => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imagemContainer}>
+        <ImageTemplate />
+      </div>
+    </div>
+  );
 };
 
 export default CardTipoProduto;
