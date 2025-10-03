d1d6233fd3b6b16cfd3c59816dd523fa6ffe9aa1
João Lucas <lgbr212@gmail.com>
2025-10-03 14:22:09 -0300

    style(cardTipoProduto): estruturando os cards do portfolio

--- src/components/cardTipoProduto/index.tsx
+++ src/components/cardTipoProduto/index.tsx
@@ -1,19 +1,24 @@
+"use client";
+
 import { StaticImageData } from "next/image";
 import styles from "./styles.module.scss";
 import React from "react";
 import { ImageIcon } from "lucide-react";
+import Image from "next/image";
+import { Check } from "lucide-react";
+import ButtonBorder from "../buttonBorder";
 
 type Props = {
   titulo: string;
   bullets: string[];
-  imagem: StaticImageData;
+  imagem?: StaticImageData;
   buttonScheme: string;
 };
 
 const ImageTemplate = () => {
   return (
     <div className={styles.imageTemplate}>
-      <ImageIcon opacity={0.4} />
+      <ImageIcon opacity={0.4} width={80} height={80} />
     </div>
   );
 };
@@ -22,7 +27,21 @@ const CardTipoProduto = ({ titulo, bullets, imagem, buttonScheme }: Props) => {
   return (
     <div className={styles.container}>
       <div className={styles.imagemContainer}>
-        <ImageTemplate />
+        {imagem ? <Image src={imagem} alt={titulo} /> : <ImageTemplate />}
+      </div>
+      <div className={styles.content}>
+        <h4>{titulo}</h4>
+        <ul>
+          {bullets.map((bullet, index) => (
+            <li key={index}>
+              <Check color="#515151" />
+              <p>{bullet}</p>
+            </li>
+          ))}
+        </ul>
+        <ButtonBorder borda={buttonScheme} color="white" theme={buttonScheme}>
+          Ver Linha Completa de {titulo}
+        </ButtonBorder>
       </div>
     </div>
   );
