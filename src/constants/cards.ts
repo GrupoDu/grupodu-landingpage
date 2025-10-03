251fc4692f7b6b117d69511ee8718b10542710de
João Lucas <lgbr212@gmail.com>
2025-10-03 14:03:14 -0300

    fix(cards): removendo valor extra no dom metal

--- src/constants/cards.ts
+++ src/constants/cards.ts
@@ -28,15 +28,6 @@ export const textosCardProdutos = {
         ],
         imagens: [],
       },
-      {
-        titulo: "Plataformas de Trabalho",
-        bullets: [
-          "Estrutura reforçada para uso em obras e reformas",
-          "Fácil de manusear mesmo com carga pesada",
-          "Disponível em diferentes modelos e pronta entrega",
-        ],
-        imagens: [],
-      },
     ],
     duferro: [{ titulos: ["", "", ""], bullets: ["", "", ""], imagens: [] }],
     carbuilt: [{ titulos: ["", "", ""], bullets: ["", "", ""], imagens: [] }],
