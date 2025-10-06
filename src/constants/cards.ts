b8946d159f959fe41a7f9429522484bbbc340810
João Lucas <lgbr212@gmail.com>
2025-10-06 14:19:26 -0300

    style(cards): alterando titulo dos tipos de produtos

--- src/constants/cards.ts
+++ src/constants/cards.ts
@@ -2,34 +2,34 @@ export const textosCardProdutos = {
   segmentos: {
     "dom-metal": [
       {
-        titulo: "Carros de Mão",
+        titulo: "Carro de Mão para Construção Civil",
         bullets: [
           "Estrutura reforçada para uso em obras e reformas",
           "Fácil de manusear mesmo com carga pesada",
           "Disponível em diferentes modelos e pronta entrega",
         ],
         imagens: [],
-        href: "comprar-carro-de-mao"
+        href: "comprar-carro-de-mao",
       },
       {
-        titulo: "Masseiras",
+        titulo: "Masseiras para Obra e Argamassa",
         bullets: [
           "Estrutura reforçada para uso em obras e reformas",
           "Fácil de manusear mesmo com carga pesada",
           "Disponível em diferentes modelos e pronta entrega",
         ],
         imagens: [],
-        href: "comprar-masseira-para-obras"
+        href: "comprar-masseira-para-obras",
       },
       {
-        titulo: "Plataformas de Trabalho",
+        titulo: "Plataforma de Trabalho para Construção",
         bullets: [
           "Estrutura reforçada para uso em obras e reformas",
           "Fácil de manusear mesmo com carga pesada",
           "Disponível em diferentes modelos e pronta entrega",
         ],
         imagens: [],
-        href: "comprar-plataformas-de-trabalho"
+        href: "comprar-plataformas-de-trabalho",
       },
     ],
     duferro: [{ titulos: ["", "", ""], bullets: ["", "", ""], imagens: [] }],
