bc1de498f7ead1685c9f183f9f9a286d9b8c4f06
João Lucas <lgbr212@gmail.com>
2025-10-09 16:58:41 -0300

    chore(categoriaProduto.ts): alterando nomes para portugues

--- src/@types/categoriaProduto.ts
+++ src/@types/categoriaProduto.ts
@@ -1,8 +1,8 @@
 import { StaticImageData } from "next/image";
 
 export interface CategoriaProduto {
-  category: string;
-  title: string;
-  description: string;
-  images: StaticImageData[];
+  categoria: string;
+  titulo: string;
+  descricao: string;
+  imagens: StaticImageData[];
 }
