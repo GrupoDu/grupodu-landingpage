49575f1ef6d5999d4f95096122ec0029b6982e2a
João Lucas <lgbr212@gmail.com>
2025-10-13 15:38:31 -0300

    style(contatos): criação de componente para contatos

---
+++ src/components/contatos/index.tsx
@@ -0,0 +1,11 @@
+import { IconType } from "react-icons";
+import styles from "./styles.module.scss";
+
+export const Contato = ({ Icon, contato }: { Icon: IconType; contato: string }) => {
+  return (
+    <div className={styles.contatosContainer}>
+      <Icon className={styles.icon} />
+      <span>{contato}</span>
+    </div>
+  );
+};
