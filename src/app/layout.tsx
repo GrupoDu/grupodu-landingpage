639c534c28ca1bae93874718516e479fa5da4c00
João Lucas <lgbr212@gmail.com>
2025-10-29 16:34:13 -0300

    feat(layout): Adicionando script do meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/footer";
 import Toast from "@/components/toast";
 import WsIcon from "@/components/wsicon";
 import ArrowUp from "@/components/arrowUp";
+import Script from "next/script";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -27,6 +28,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <head>
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1528143071557219');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1528143071557219&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body className={`${poppins.variable}`}>
         <Toast />
         <Navbar />
