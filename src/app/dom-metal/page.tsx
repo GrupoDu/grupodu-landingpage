07e8007fbfb9f1988a22d2ee45add9949765d320
João Lucas <lgbr212@gmail.com>
2025-11-25 14:17:05 -0300

    style(dom-metal): Removendo icone não utilizado

--- src/app/dom-metal/page.tsx
+++ src/app/dom-metal/page.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import styles from "./page.module.scss";
 import Image from "next/image";
-import { LuCheck, LuImage } from "react-icons/lu";
+import { LuCheck } from "react-icons/lu";
 import { IconType } from "react-icons";
 import Button from "@/components/button";
 import ListaCatalogo from "@/components/listaCatalogo";
