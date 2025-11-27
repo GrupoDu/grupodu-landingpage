81b62310a7b513cb7d8a96cd321ae5e3285670d9
João Lucas <lgbr212@gmail.com>
2025-11-27 14:43:24 -0300

    feat(contatosSection): Adicionando funcionalidade de baixar catalogo no botão 'Catálogo Completo'

--- src/components/contatosSection/index.tsx
+++ src/components/contatosSection/index.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import styles from "./styles.module.scss";
-import React from 'react'
+import React, { useEffect } from "react";
 import { contatos } from "@/constants/contatos";
 import { Contato } from "@/components/contatos";
 import ButtonDecoration from "@/components/buttonDecoration";
@@ -8,46 +10,50 @@ import FormProduto from "@/components/formProduto";
 import { MdKeyboardArrowUp } from "react-icons/md";
 
 const ContatosSection = () => {
+  const downloadCatalogo = () => {
+    const baseUrl = window.location.origin;
+    const link = document.createElement("a");
+    link.href = `${baseUrl}/catalogo.pdf`;
+    link.download = "Catálogo Grupo Du.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className={styles.formularioContato}>
-          <div className={styles.contatos}>
-            <h2>Solicite seu Produto</h2>
-            <p>
-              Preencha seus dados e nossa equipe entrará em contato para
-              garantir que você receba exatamente o que procura.
-            </p>
-            <hr />
-            <h3>Outras formas de contato</h3>
-            {contatos.map((contato, index) => (
-              <Contato
-                key={index}
-                Icon={contato.icon}
-                contato={contato.contato}
-              />
-            ))}
-            <div className={styles.buttons}>
-              <span>Ainda não conhece nossos produtos?</span>
-              <ButtonDecoration
-                type="link"
-                href="#"
-                borda="#201750"
-                theme="#201750"
-                color="white"
-                borderRadius="4px"
-              >
-                Catálogo completo
-              </ButtonDecoration>
-              <Button
-                type="link"
-                href="#produtos"
-              >
-                Ver Linha Completa de Carros de Mão <MdKeyboardArrowUp />
-              </Button>
-            </div>
-          </div>
-          <FormProduto />
+      <div className={styles.contatos}>
+        <h2>Solicite seu Produto</h2>
+        <p>
+          Preencha seus dados e nossa equipe entrará em contato para garantir
+          que você receba exatamente o que procura.
+        </p>
+        <hr />
+        <h3>Outras formas de contato</h3>
+        {contatos.map((contato, index) => (
+          <Contato key={index} Icon={contato.icon} contato={contato.contato} />
+        ))}
+        <div className={styles.buttons}>
+          <span>Ainda não conhece nossos produtos?</span>
+          <ButtonDecoration
+            type="button"
+            href="#"
+            borda="#201750"
+            theme="#201750"
+            color="white"
+            borderRadius="4px"
+            click={downloadCatalogo}
+          >
+            Catálogo completo
+          </ButtonDecoration>
+          <Button type="link" href="#produtos">
+            Ver Linha Completa de Carros de Mão <MdKeyboardArrowUp />
+          </Button>
         </div>
-  )
-}
+      </div>
+      <FormProduto />
+    </div>
+  );
+};
 
-export default ContatosSection
+export default ContatosSection;
