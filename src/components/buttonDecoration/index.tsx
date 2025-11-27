9758d7fb51802206135f0b734981af8ff77cc5c0
João Lucas <lgbr212@gmail.com>
2025-11-27 15:54:38 -0300

    style(buttonDecoration): Ajustando forma que as props são chamadas na função

--- src/components/buttonDecoration/index.tsx
+++ src/components/buttonDecoration/index.tsx
@@ -15,55 +15,46 @@ type Props = {
   click?: () => void;
 };
 
-const ButtonDecoration = ({
-  children,
-  theme,
-  borda,
-  color,
-  borderRadius,
-  href,
-  type,
-  click,
-}: Props) => {
+const ButtonDecoration = (props: Props) => {
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseEnter = () => setIsHovered(true);
   const handleMouseLeave = () => setIsHovered(false);
 
-  if (type === "link") {
+  if (props.type === "link") {
     return (
       <Link
-        href={href}
+        href={props.href}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
         className={styles.button}
         style={{
-          backgroundColor: isHovered ? theme : "white",
-          border: borda ? `1px solid ${borda}` : "none",
-          color: isHovered ? color : "black",
-          borderRadius: borderRadius ? borderRadius : "0px",
+          backgroundColor: isHovered ? props.theme : "white",
+          border: props.borda ? `1px solid ${props.borda}` : "none",
+          color: isHovered ? props.color : "black",
+          borderRadius: props.borderRadius ? props.borderRadius : "0px",
         }}
       >
-        {children}
+        {props.children}
       </Link>
     );
   }
 
-  if (type === "button") {
+  if (props.type === "button") {
     return (
       <button
-        onClick={click}
+        onClick={props.click}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
         className={styles.button}
         style={{
-          backgroundColor: isHovered ? theme : "white",
-          border: borda ? `1px solid ${borda}` : "none",
-          color: isHovered ? color : "black",
-          borderRadius: borderRadius ? borderRadius : "0px",
+          backgroundColor: isHovered ? props.theme : "white",
+          border: props.borda ? `1px solid ${props.borda}` : "none",
+          color: isHovered ? props.color : "black",
+          borderRadius: props.borderRadius ? props.borderRadius : "0px",
         }}
       >
-        {children}
+        {props.children}
       </button>
     );
   }
