d4286a378a2d39ecb7f423166354f0353ec40f0f
João Lucas <lgbr212@gmail.com>
2025-12-02 14:31:02 -0300

    feat(validateInputsValues): Função para validar se os campos foram preenchidos

---
+++ src/utils/validateInputsValues.ts
@@ -0,0 +1,12 @@
+export function validateInputsValues(inputsValues: IContactInfos) {
+  const { email, name, phone, company, message, subject } = inputsValues;
+  let toastMessage = "";
+
+  if (!email || !name || !phone || !company || !message || !subject) {
+    toastMessage = "Todos os campos devem ser preenchidos!";
+    return [false, toastMessage];
+  } else {
+    toastMessage = `Mensagem enviada para ${email}`;
+    return [true, toastMessage];
+  }
+}
