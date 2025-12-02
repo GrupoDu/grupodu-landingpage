32beebc02d641780be4e4f286ac7efad494f930b
João Lucas <lgbr212@gmail.com>
2025-12-02 14:17:13 -0300

    remove: Removendo hooks e transformando em utils

--- src/hooks/useToast.ts
+++
@@ -1,5 +0,0 @@
-import toast from "react-hot-toast"
-
-export const useToast = ({ toastType, message } : { toastType: "success" | "error", message: string}) => {
-    return toast[toastType](message);
-}
