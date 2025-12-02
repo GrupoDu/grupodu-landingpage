3a75cd227fb925efb308c8cfb27cf5ef8fc2269f
João Lucas <lgbr212@gmail.com>
2025-12-02 15:43:37 -0300

    refactor: Movendo global.d.ts para pasta root

---
+++ global.d.ts
@@ -0,0 +1,34 @@
+declare module "swiper/css";
+declare module "swiper/css/navigation";
+declare module "swiper/css/pagination";
+declare module "swiper/css/scrollbar";
+
+declare module "*.module.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "@/app/globals.scss" {
+  const content: string;
+  export default content;
+}
+
+declare global {
+  interface IUserInfos {
+    email: string;
+    name: string;
+    phone: string;
+    company: string;
+  }
+
+  interface Produto {
+    id: number;
+    nome: string;
+    descricao: string;
+    tipo_produto: string;
+    imagem: StaticImageData;
+    caracteristicas: string[];
+  }
+}
+
+export {};
