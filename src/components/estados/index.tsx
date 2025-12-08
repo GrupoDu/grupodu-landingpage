a12b2185f4f65c8a664dc9b35e6f9236267bd894
João Lucas <lgbr212@gmail.com>
2025-12-08 13:53:42 -0300

    refactor(estados): Importando lista de estados

--- src/components/estados/index.tsx
+++ src/components/estados/index.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import Image from "next/image";
+import { statesList } from "@/constants/statesList";
 
 //Imagem
 import MapaEstados from "../../../public/mapa-estados.png";
 
 const Estados = () => {
-  const estados = [
-    "Alagoas - Maceió (AL)",
-    "Bahia - Salvador (BA)",
-    "Ceará - Fortaleza (CE)",
-    "Maranhão - São Luís (MA)",
-    "Paraíba - João Pessoa (PB)",
-    "Pernambuco - Recife (PE)",
-    "Piauí - Teresina (PI)",
-    "Rio Grande Do Norte - Natal (RN)",
-    "Sergipe - Aracaju(SE)",
-  ];
-
   return (
     <div className={styles.estadosContainer}>
       <Image
@@ -29,7 +18,7 @@ const Estados = () => {
       <div className={styles.listContainer}>
         <h1>Atendemos em todo o nordeste</h1>
         <ul>
-          {estados.map((estado, index) => (
+          {statesList.map((estado, index) => (
             <li key={index}>{estado}</li>
           ))}
         </ul>
