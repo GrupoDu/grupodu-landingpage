f263acb15a4507fd8b90700735046ab1ca1d08b8
João Lucas <lgbr212@gmail.com>
2025-12-08 13:53:09 -0300

    feat(statesList): Separando lista de estados

---
+++ src/constants/statesList.ts
@@ -0,0 +1,11 @@
+export const statesList = [
+  "Alagoas - Maceió (AL)",
+  "Bahia - Salvador (BA)",
+  "Ceará - Fortaleza (CE)",
+  "Maranhão - São Luís (MA)",
+  "Paraíba - João Pessoa (PB)",
+  "Pernambuco - Recife (PE)",
+  "Piauí - Teresina (PI)",
+  "Rio Grande Do Norte - Natal (RN)",
+  "Sergipe - Aracaju(SE)",
+];
