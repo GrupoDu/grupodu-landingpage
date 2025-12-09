0262f760807f05b687bd6ffa4875b99f98ab17bc
João Lucas <lgbr212@gmail.com>
2025-12-09 16:02:18 -0300

    feat(filtersMobile): Adicionando função de abrir os filtros

--- src/components/filtersComponents/filtersMobile/index.tsx
+++ src/components/filtersComponents/filtersMobile/index.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import { FaFilter } from "react-icons/fa";
 import styles from "./styles.module.scss";
+import { useState } from "react";
 
-const FiltersMobile = () => {
+const FiltersMobile = ({
+  openFilterClickEvent,
+}: {
+  openFilterClickEvent: () => void;
+}) => {
   return (
     <div className={styles.filtersMobileContainer}>
-      <div className={styles.filterButton}>
+      <div
+        onClick={() => openFilterClickEvent()}
+        className={styles.filterButton}
+      >
         <FaFilter color="white" />
         <span>Filtros</span>
       </div>
