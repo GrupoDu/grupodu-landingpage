668bd4fe22605c3fa4514d86eb4b5987cad536fa
João Lucas <lgbr212@gmail.com>
2025-12-09 16:07:20 -0300

    style: Movendo filterMobileContainer para a pasta de filtros

---
+++ src/components/filtersComponents/filterMobileContainer/index.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import styles from "./styles.module.scss";
+import { productsFiltersList } from "@/constants/productsFiltersList";
+import FilterBlock from "../filtersComponents/filterBlock";
+import FiltersMobile from "../filtersComponents/filtersMobile";
+import { useHandleOpenFilterMobile } from "@/hooks/useHandleOpenFilterMobile";
+import Button from "../ui/buttons/button";
+import { clearProductsFilter } from "@/utils/clearProductsFilter";
+import { usePathname } from "next/navigation";
+
+const FilterMobileContainer = () => {
+  const { filterIsOpen, openFilterMobile, closeFilterMobile } =
+    useHandleOpenFilterMobile();
+  const pathname = usePathname();
+
+  return (
+    <>
+      <FiltersMobile openFilterClickEvent={openFilterMobile} />
+      <div
+        className={`${styles.filterBlockMobile} ${filterIsOpen && styles.filterMobileOpen}`}
+      >
+        <FilterBlock filters={productsFiltersList[0]} />
+        <Button type="button" click={() => clearProductsFilter(pathname)}>
+          Limpar Filtros
+        </Button>
+        <Button type="button" click={() => closeFilterMobile()}>
+          Fechar filtros
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default FilterMobileContainer;
